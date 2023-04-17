6e02fa4e9e61777a9de2ce54140b16d64b23a46e
TianyiLi770617 <131078938+TianyiLi770617@users.noreply.github.com>
2023-04-17 19:56:20 -0400

    Add files via upload

---
+++ Question15.cs
@@ -0,0 +1,43 @@
+﻿namespace NetFinalTestCodes
+{
+
+    // Define a generic class that constrains the developer to only instantiate the class based on value types
+    public class ValuesCollection<T> where T : struct, IComparable<T>
+    {
+        // Declare a private generic collection
+        private List<T> _items;
+
+        // Define a constructor that initializes the private collection
+        public ValuesCollection()
+        {
+            _items = new List<T>();
+        }
+
+        // Define a method to add items to the private collection
+        public void AddItem(T item)
+        {
+            _items.Add(item);
+        }
+
+        // Define a method that will return an item from the list
+        public T GetItem(int index)
+        {
+            if (index >= 0 && index < _items.Count)
+            {
+                return _items[index];
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+            }
+        }
+
+        // Define a method that returns a sorted collection with the items in descending order
+        public IEnumerable<T> GetSortedDescending()
+        {
+            return _items.OrderByDescending(x => x);
+        }
+
+
+    }
+}
---
+++ Question16.cs
@@ -0,0 +1,59 @@
+﻿namespace NetFinalTestCodes
+{
+    
+    // 1) Create a class to pass as an argument for the event handlers (EventArgs class)
+    public class WeightChangedEventArgs : EventArgs
+    {
+        public double OldWeight { get; }
+        public double NewWeight { get; }
+
+        public WeightChangedEventArgs(double oldWeight, double newWeight)
+        {
+            OldWeight = oldWeight;
+            NewWeight = newWeight;
+        }
+    }
+
+    public class WeightSensor
+    {
+        // 2) Set up the delegate for the event
+        public delegate void WeightChangedEventHandler(object sender, WeightChangedEventArgs e);
+
+        // 3) Declare the code for the event
+        public event WeightChangedEventHandler WeightChanged;
+
+        private double _Weight;
+
+        public double Weight
+        {
+            get => _Weight;
+            set
+            {
+                if (_Weight != value)
+                {
+                    double oldWeight = _Weight;
+                    _Weight = value;
+                    // Trigger the event
+                    WeightChanged?.Invoke(this, new WeightChangedEventArgs(oldWeight, _Weight));
+                }
+            }
+        }
+    }
+
+    public class WeightMonitor
+    {
+        public WeightMonitor(WeightSensor sensor)
+        {
+            // 5) Associate the event with the event handler
+            sensor.WeightChanged += OnWeightChanged;
+        }
+
+        // 4) Create code that will be run when the event occurs (Event Handler)
+        private void OnWeightChanged(object sender, WeightChangedEventArgs e)
+        {
+            Console.WriteLine($"Weight changed from {e.OldWeight} to {e.NewWeight}");
+        }
+    }
+
+
+}
